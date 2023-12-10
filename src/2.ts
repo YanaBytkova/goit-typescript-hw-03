cc2f509ba6a63e76fbb1817a2aab2e0017459e53
YanaBytkova <nedavnyaya.yana@gmail.com>
2023-12-10 20:16:16 +0200

    first

--- src/2.ts
+++ src/2.ts
@@ -16,11 +16,9 @@
 
 class Employee {
   // Заповніть модифікатори доступу
-  name: string;
-  department: string;
-  salary: number;
+ 
 
-  constructor(name: string, department: string, salary: number) {
+  constructor( public name: string, private department: string, private salary: number) {
     this.name = name;
     this.department = department;
     this.salary = salary;
@@ -33,7 +31,13 @@ class Employee {
 
 class Manager extends Employee {
   // Реалізуйте конструктор та збільшіть salary на 10000
+  constructor(name: string, department: string, salary: number) {
+    super(name, department, salary);
+  
+    const newSalary = salary * 10000;
+  }
 }
 
 
 export {};
+
